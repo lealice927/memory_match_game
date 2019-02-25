cd53a2351b423ac5b9bfe453830e3b3ac61a4653
Alice Le <lealice@gmail.com>
2019-02-24 18:56:12 -0800

    Memory Match v1.0

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 $(document).ready(initializeGame);
 
 function initializeGame() {
-    console.log('Initializing Game...');
-
+    //console.log('Initializing Game...');
     $('.card').on('click', card_clicked);
+    games_played++;
 }
+
+
+// function addEventHandlers() {
+//     console.log('event listener')
+//     $('.card').click(handleClick);
+// }
+// //     $('.card').on('click', card_clicked);
+// //  when we have a lot of event handlers like clicks/reset buttons
+// function handleClick(){
+    
+// }
 
 var first_card_clicked = null;
 var second_card_clicked = null;
-var total_possible_matches = 2;
-var match_counter = 0;
-var front_card = [];
-var winner = console.log("Winner!")
-
-
+var total_possible_matches = 9;
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+var games_played = 0;
 
 function card_clicked() {
     $(this).find('.back').hide();
@@ -21,32 +31,67 @@ function card_clicked() {
     if (first_card_clicked === null) {
         first_card_clicked = $(this).html();
         first_card_back = $(this).find('.back');
-        return;
-    } else {
+        // return;
+    } else if (second_card_clicked === null) {
         second_card_clicked = $(this).html();
         second_card_back = $(this).find('.back');
+        // console.log('# of Attempts:', attempts);
+        attempts++;
+
         //  debugger;
         if (first_card_clicked === second_card_clicked) {
-            match_counter++;
+            // console.log('# of matches:', matches);
+            matches++;
             first_card_clicked = null;
             second_card_clicked = null;
-            if (match_counter === total_possible_matches) {
-                return winner;
+
+            if (matches === total_possible_matches) {
+                display_stats();
+                alert("YOU MATCHED ALL THE CARDS!!!");
             } else {
+                display_stats();
                 return;
             }
         } else {
-            // debugger;
-            setTimeout(function() {
-               first_card_back.show();
-               second_card_back.show();
-            }, 2000);
+            display_stats();
+            setTimeout(function () {
+            $('.card').off('click', card_clicked);
+                first_card_back.show();
+                second_card_back.show();
+             $('.card').on('click', card_clicked);
+            }, 1000);
             first_card_clicked = null;
             second_card_clicked = null;
             return;
-            
         }
-    } 
-    
+    }
 }
-    
+// debugger;
+// STATS PORTION //
+function display_stats() {
+    //console.log('Display Stats:', display_stats);
+    $('.games_played .value').text(games_played);
+    $('.attempts .value').text(attempts);
+    $('.matches .value').text(matches);
+    if (attempts === 0) {
+        accuracy = 0 + '%';
+    } else {
+        accuracy = (Math.floor((matches / attempts) * 100)) + '%';
+        accuracy;
+    } 
+    $('.accuracy .value').text(accuracy);
+}
+
+function reset_stats() {
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+    display_stats();
+}
+
+function resetButton() {
+    games_played++;
+    reset_stats();
+    display_stats();
+    $('.card').find('.back').show();
+}
