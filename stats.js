ce54a999eaf69b788ac06451734cdc978c874a21
Alice Le <lealice@gmail.com>
2019-07-17 22:58:46 -0700

    added function to display stats

--- stats.js
+++ stats.js
@@ -13,20 +13,33 @@ class Stats {
         $('.description .label').text('Description of Shark:')
         $('.description .value').text(' ');
         $('.description .fun-fact').text(' ');
-      }
+    }
 
-      addDescription( sharksObj, secondCardClicked ) {
+    addDescription(sharksObj, secondCardClicked) {
         $('.description .label').text('')
         var currentImagePath = $(secondCardClicked.cardFront).css('background-image');
         var firstIndex = currentImagePath.indexOf('image');
         var lastIndex = currentImagePath.lastIndexOf('\")');
         var currentImage = currentImagePath.slice(firstIndex, lastIndex);
-    
-        for(var sharks in sharksObj) {
-          if(currentImage === sharksObj[shark].link) {
-            $('.description .fun-fact').text(sharksObj[sharks].sharks_description);
-            $('.description .value').text(sharks);
-          }
+
+        for (var sharks in sharksObj) {
+            if (currentImage === sharksObj[shark].link) {
+                $('.description .fun-fact').text(sharksObj[sharks].sharks_description);
+                $('.description .value').text(sharks);
+            }
         }
+    }
+
+    displayStats() {
+        $('.attempts .value').text(this.attempts);
+    
+        if(this.accuracy === 'NaN%') {
+          this.accuracy = '0.00%';
+        }
+        $('.accuracy .value').text(this.accuracy);
       }
+
+
+
+
 }
